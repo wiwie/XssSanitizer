7c7bbd58f01be51efcff90683ede29c47d93f2c0
Christian Wiwie <wiwiec@imada.sdu.dk>
2018-10-30 10:02:37 +0100

    added missing ESAPI files; making use of ESAPI encoding instead of custom solution based on regex replace;

--- src/java/org/tonyzampogna/xss/sanitizer/util/XssSanitizerUtil.java
+++ src/java/org/tonyzampogna/xss/sanitizer/util/XssSanitizerUtil.java
@@ -10,56 +10,6 @@ import java.util.regex.Pattern;
  */
 public class XssSanitizerUtil {
 
-	private static List<Pattern> XSS_INPUT_PATTERNS = new ArrayList<Pattern>();
-
-	static {
-			// Avoid anything between script tags
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE));
-
-			// avoid iframes
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<iframe(.*?)>(.*?)</iframe>", Pattern.CASE_INSENSITIVE));
-
-			// avoid inputs
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<input(.*?)>(.*?)</input>", Pattern.CASE_INSENSITIVE));
-
-			// Avoid anything in a src='...' type of expression
-			XSS_INPUT_PATTERNS.add(Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			XSS_INPUT_PATTERNS.add(Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			XSS_INPUT_PATTERNS.add(Pattern.compile("src[\r\n]*=[\r\n]*([^>]+)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Remove any lonesome </script> tag
-			XSS_INPUT_PATTERNS.add(Pattern.compile("</script>", Pattern.CASE_INSENSITIVE));
-
-			// Remove any lonesome <script ...> tag
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Remove any lonesome <input ...> tag
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<input(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Avoid eval(...) expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Avoid expression(...) expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Avoid javascript:... expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE));
-
-			// Avoid vbscript:... expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE));
-
-			// Avoid onload= expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-			 
-			// Avoid onfocus= expressions
-			XSS_INPUT_PATTERNS.add(Pattern.compile("onfocus(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-
-			// Avoid any form injection with <...form ...> ... </form ...> tag
-			XSS_INPUT_PATTERNS.add(Pattern.compile("<(.*?)form(.*?)>(.*?)</(.*?)form(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
-	}
-
 	/**
 	 * This method takes a string and strips out any potential script injections.
 	 *
@@ -74,14 +24,13 @@ public class XssSanitizerUtil {
 				// NOTE: It's highly recommended to use the ESAPI library and uncomment the following line to
 				// avoid encoded attacks.
 				value = ESAPI.encoder().canonicalize(value);
+				
+				// Use ESAPI library to encode request parameters for use as html attributes
+				value = ESAPI.encoder().encodeForHTMLAttribute(value);
+//				value = ESAPI.encoder().encodeForJavaScript(value);
 
 				// Avoid null characters
 				value = value.replaceAll("\0", "");
-
-				// test against known XSS input patterns
-				for (Pattern xssInputPattern : XSS_INPUT_PATTERNS) {
-					value = xssInputPattern.matcher(value).replaceAll("");
-				}
 			}
 
 		} catch (Exception ex) {
--- test/unit/org/tonyzampogna/xss/sanitizer/util/XssSanitizerUtilTest.java
+++ test/unit/org/tonyzampogna/xss/sanitizer/util/XssSanitizerUtilTest.java
@@ -21,72 +21,78 @@ public class XssSanitizerUtilTest {
     @Test
     public void shouldStripOutNullCharacters() {
         final String output = XssSanitizerUtil.stripXSS("\0");
-        assertThat(output, is(""));
+        assertThat(output, is("&#xfffd;"));
     }
 
     @Test
     public void shouldStripOutContentOfScriptTags() {
         final String output = XssSanitizerUtil.stripXSS("valid-content<script>xss-content</script>");
-        assertThat(output, is("valid-content"));
+        assertThat(output, is("valid-content&lt;script&gt;xss-content&lt;&#x2f;script&gt;"));
     }
 
     @Test
     public void shouldStripOutTrailingScriptTag() {
         final String output = XssSanitizerUtil.stripXSS("</script>valid-content");
-        assertThat(output, is("valid-content"));
+        assertThat(output, is("&lt;&#x2f;script&gt;valid-content"));
     }
 
     @Test
     public void shouldStripOutStartingScriptTag() {
         final String output = XssSanitizerUtil.stripXSS("<script src='xss.js'>valid-content");
-        assertThat(output, is("valid-content"));
+        assertThat(output, is("&lt;script&#x20;src&#x3d;&#x27;xss.js&#x27;&gt;valid-content"));
     }
 
     @Test
     public void shouldStripOutEvalAttribute() {
         final String output = XssSanitizerUtil.stripXSS("eval('xss-js-content')");
-        assertThat(output, is(""));
+        assertThat(output, is("eval&#x28;&#x27;xss-js-content&#x27;&#x29;"));
     }
 
     @Test
     public void shouldStripOutExpressionAttribute() {
         final String output = XssSanitizerUtil.stripXSS("expression('xss-content')");
-        assertThat(output, is(""));
+        assertThat(output, is("expression&#x28;&#x27;xss-content&#x27;&#x29;"));
     }
 
     @Test
     public void shouldStripOutOnloadAttribute() {
         final String output = XssSanitizerUtil.stripXSS("onload=xss.execute()");
-        assertThat(output, is("xss.execute()"));
+        assertThat(output, is("onload&#x3d;xss.execute&#x28;&#x29;"));
     }
 
     @Test
     public void shouldStripOutJavascriptProtocol() {
         final String output = XssSanitizerUtil.stripXSS("javascript:xss.execute()");
-        assertThat(output, is("xss.execute()"));
+        assertThat(output, is("javascript&#x3a;xss.execute&#x28;&#x29;"));
     }
 
     @Test
     public void shouldStripOutVbcriptProtocol() {
         final String output = XssSanitizerUtil.stripXSS("vbscript:xss.execute()");
-        assertThat(output, is("xss.execute()"));
+        assertThat(output, is("vbscript&#x3a;xss.execute&#x28;&#x29;"));
     }
 
     @Test
     public void shouldStripOutSrcAttribute() {
         final String output = XssSanitizerUtil.stripXSS("<img src='xss.jpg'>");
-        assertThat(output, is("<img >"));
+        assertThat(output, is("&lt;img&#x20;src&#x3d;&#x27;xss.jpg&#x27;&gt;"));
     }
 
     @Test
     public void shouldStripOutContentOfIframeTags() {
         final String output = XssSanitizerUtil.stripXSS("<iframe src='xss.html'>xss-content</iframe>");
-        assertThat(output, is(""));
+        assertThat(output, is("&lt;iframe&#x20;src&#x3d;&#x27;xss.html&#x27;&gt;xss-content&lt;&#x2f;iframe&gt;"));
     }
 
     @Test
-    public void shouldStripOutContentOfIframeTags() {
+    public void shouldStripOutContentOfForm() {
         final String output = XssSanitizerUtil.stripXSS("<form action=''><input id='formInjection'></form>");
-        assertThat(output, is(""));
+        assertThat(output, is("&lt;form&#x20;action&#x3d;&#x27;&#x27;&gt;&lt;input&#x20;id&#x3d;&#x27;formInjection&#x27;&gt;&lt;&#x2f;form&gt;"));
+    }
+
+    @Test
+    public void shouldEscapeDoubleQuote() {
+        final String output = XssSanitizerUtil.stripXSS("\"asd");
+        assertThat(output, is("&quot;asd"));
     }
 }
